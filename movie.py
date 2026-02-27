bcbf59b903087a457b1dc5d8ff3e846b9e87296d
LCapistrano25 <leonardo.capistrano445@gmail.com>
2026-02-27 12:16:17 -0300

    refactor: move GENRE_CODES de MovieNode para módulo de constantes
    Remove valores hardcoded e melhora organização do domínio.

--- movie.py
+++ movie.py
@@ -1,14 +1,6 @@
 from node import Node
-
-GENRE_CODES = {
-    "Action": 1,
-    "Animação": 2,
-    "Crime": 3,
-    "Drama": 4,
-    "Fantasy": 5,
-    "Thriller": 6,
-}
-
+from constants import GENRE_CODES
+        
 class MovieNode(Node):
     def __init__(self, title, genre, rating, year):
         super().__init__(year)  # Utilize o ano de lançamento como chave na árvore binária
