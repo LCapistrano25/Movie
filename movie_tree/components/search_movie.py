d16a002320c36601c70eae60b7d31ae9afbe9d1e
LCapistrano25 <leonardo.capistrano445@gmail.com>
2026-02-27 14:57:34 -0300

    feat: adicionar a classe SearchMovie para buscar filmes em uma árvore binária.
    Implementar um método estático para buscar recursivamente nós que correspondam a um campo/valor.

---
+++ movie_tree/components/search_movie.py
@@ -0,0 +1,16 @@
+from node import Node
+
+class SearchMovie:
+    @staticmethod
+    def search_movies(root: Node, field: str, value: str) -> list:
+        if root is None:
+            return []
+
+        movies = []
+        if getattr(root, field) == value:
+            movies.append(root)
+
+        movies += SearchMovie.search_movies(root.left, field, value)
+        movies += SearchMovie.search_movies(root.right, field, value)
+
+        return movies
