aaebc9149015c9edf4e6eb12bd7649581df0acbd
LCapistrano25 <leonardo.capistrano445@gmail.com>
2026-02-27 14:20:44 -0300

    refactor: move MovieAdd para módulo components/movie_add
    Reorganiza estrutura para melhorar separação de responsabilidades.

---
+++ movie_tree/components/movie_add.py
@@ -0,0 +1,32 @@
+from movie import MovieNode
+
+class MovieAdd:
+    
+    @staticmethod
+    def add_movie(root: MovieNode, title: str, genre: str, rating: float, year: int):
+        new_movie = MovieNode(title, genre, rating, year)
+        root = MovieAdd._insert(root, new_movie)
+        return root
+    
+    @staticmethod
+    def _insert(root: MovieNode = None, new_movie: MovieNode = None):
+        if root is None:
+            return new_movie
+
+        if root.year > new_movie.year:
+            root.left = MovieAdd._insert(root.left, new_movie)
+        elif root.year < new_movie.year:
+            root.right = MovieAdd._insert(root.right, new_movie)
+        else:
+            # Em caso de anos iguais, comparar por código de gênero
+            if root.genre_code > new_movie.genre_code:
+                root.left = MovieAdd._insert(root.left, new_movie)
+            elif root.genre_code < new_movie.genre_code:
+                root.right = MovieAdd._insert(root.right, new_movie)
+            else:
+                # Em caso de gênero e ano iguais, comparar por classificação
+                if root.rating > new_movie.rating:
+                    root.left = MovieAdd._insert(root.left, new_movie)
+                else:
+                    root.right = MovieAdd._insert(root.right, new_movie)
+        return root
