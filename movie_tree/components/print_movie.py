a85dece5e44e1d2202e7365733a9aa845386e28a
LCapistrano25 <leonardo.capistrano445@gmail.com>
2026-02-27 14:22:03 -0300

    feat: adiciona classe PrintMovie no módulo print_movie
    Implementa responsabilidade de exibir dados do filme.

---
+++ movie_tree/components/print_movie.py
@@ -0,0 +1,31 @@
+from node import Node
+
+class PrintMovie:
+
+    @staticmethod
+    def print_tree(root: Node = None, side: str = "") -> None:
+        if root is None:
+            return
+        
+        PrintMovie.print_tree(root.left, "l")
+        print(root.title, "-", root.genre, "-", root.rating, "-", root.year, "-", root.genre_code, "-", side)
+        PrintMovie.print_tree(root.right, "r")
+
+
+    @staticmethod
+    def print_tree_pre(root: Node = None) -> None:
+        if root is None:
+            return
+        
+        print(root.title, "-", root.genre, "-", root.rating, "-", root.year)
+        PrintMovie.print_tree_pre(root.left)
+        PrintMovie.print_tree_pre(root.right)
+
+    @staticmethod
+    def print_tree_pos(root: Node = None) -> None:
+        if root is None:
+            return
+        
+        PrintMovie.print_tree_pos(root.left)
+        PrintMovie.print_tree_pos(root.right)
+        print(root.title, "-", root.genre, "-", root.rating, "-", root.year)
